087be7f72e739904b3aba6e53d14131295f1c776
azinoreva <azinoreva@gmail.com>
2023-08-10 13:16:11 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h> 
 /**
- *  main - A program that prints a statement
+ * main - A program that prints a statement
  * Return: 0
  */
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0;}
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
- *  main - A program that prints a statement
- *  Return: 0
+ * main - A program that prints a statement
+ * Return: 0
  */
-int main(void) {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
